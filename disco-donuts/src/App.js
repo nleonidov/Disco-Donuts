d4dd7b478b06db2c75badd1a8ca3e8cd57a005b8
Nick Leonidov <nickleonidov@Nicks-MacBook-Air.local>
2020-10-09 16:59:36 -0400

    airtable setup for state

--- disco-donuts/src/App.js
+++ disco-donuts/src/App.js
@@ -1,10 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link, Route } from "react-router-dom";
+import { baseURL } from "./Constants";
+import axios from "axios";
 import Home from "./Home";
 import Order from "./Order";
 import "./Style.css";
 
 function App() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const getProducts = async () => {
+      const airtableURL = `${baseURL}`;
+      const response = await axios.get(airtableURL, {
+        headers: {
+          Authorization: `Bearer ${process.env.REACT_APP_AIRTABLE_KEY}`,
+        },
+      });
+      setProducts(response.data);
+    };
+    getProducts();
+  }, []);
+
   return (
     <>
       <nav>
